cd5885fdd195002d56bdd7bdf914db636476a848
Timur R <timurreziapov2@gmail.com>
2022-11-23 12:30:51 -0800

    Adds clickHandler func to button

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const clickMeB = document.querySelector(".clickme");
+// console.log(clickMeB);
+
+function clickHandler () {
+    alert("Downloading virus...");
+}
+
+clickMeB.addEventListener("click",clickHandler);
